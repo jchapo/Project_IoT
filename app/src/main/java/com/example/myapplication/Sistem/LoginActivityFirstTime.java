dd43375d29857daad680b50dfea4258145601d87
Juan <jchaponan@pucp.pe>
2024-05-06 17:06:23 -0500

    Avance 28 Juan

--- app/src/main/java/com/example/myapplication/Sistem/LoginActivityFirstTime.java
+++ app/src/main/java/com/example/myapplication/Sistem/LoginActivityFirstTime.java
@@ -1,13 +1,19 @@
 package com.example.myapplication.Sistem;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivityFirstTime extends AppCompatActivity {
 
@@ -27,6 +33,48 @@ public class LoginActivityFirstTime extends AppCompatActivity {
             }
         });
 
+        TextView textForgotPassword = findViewById(R.id.textForgotPasswordFirstTime); // Reemplaza "textForgotPassword" con el ID correcto de tu TextView
+        textForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showForgotPasswordPopup();
+            }
+        });
+
+    }
+
+    private void showForgotPasswordPopup() {
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+        builder.setTitle("Correo de recuperación:");
+
+        final TextInputLayout textInputLayout = new TextInputLayout(this);
+        textInputLayout.setHint("Correo electrónico");
+        int padding = (int) getResources().getDimension(R.dimen.padding_16dp);
+        textInputLayout.setPadding(padding, padding, padding, padding);
+
+        final TextInputEditText textInputEditText = new TextInputEditText(this);
+        textInputLayout.addView(textInputEditText);
+
+        builder.setView(textInputLayout);
+
+        builder.setPositiveButton("Enviar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String userEmail = textInputEditText.getText().toString();
+                Toast.makeText(getBaseContext(), "Correo electrónico enviado", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
     }
 
 
