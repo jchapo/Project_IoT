82fe8307410ebfbca2576cc96118860129e597a4
Juan <jchaponan@pucp.pe>
2024-06-02 20:57:47 -0500

    Avance 55: Implementación subida de imagenes Sitios

--- app/src/main/java/com/example/myapplication/Admin/MainActivity_2_Sites_NewSite.java
+++ app/src/main/java/com/example/myapplication/Admin/MainActivity_2_Sites_NewSite.java
@@ -50,11 +50,13 @@ import java.io.ByteArrayOutputStream;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity_2_Sites_NewSite extends AppCompatActivity {
     String canal1 = "importanteDefault3";
     ListElementSite element;
-    private EditText editAddress, editUbigeo, editSiteCoordenadas;
+    private EditText editAddress, editUbigeo, editSiteCoordenadas, editDistrict;
     double latitude, longitude;
     String nombredesitio;
     private static final int PICK_IMAGE_REQUEST = 1;
@@ -62,8 +64,8 @@ public class MainActivity_2_Sites_NewSite extends AppCompatActivity {
     private boolean isEditing = false; // Indicador para editar o crear nuevo sitio
     private boolean isImageAdded = false; // Variable para indicar si se ha agregado una imagen
     private Uri imageUri;
-    private MaterialAutoCompleteTextView selectDepartment, selectProvince, selectDistrict, selectZoneType, selectSiteType;
-    ArrayAdapter<String> departmentAdapter,provinceAdapter,districtAdapter,zoneTypeAdapter, siteTypeAdapter;
+    private MaterialAutoCompleteTextView selectDepartment, selectProvince, selectZoneType, selectSiteType;
+    ArrayAdapter<String> departmentAdapter,provinceAdapter,zoneTypeAdapter, siteTypeAdapter;
 
     String[] departmentOptions = {
             "Amazonas", "Áncash","Apurímac","Arequipa","Ayacucho","Cajamarca","Callao","Cusco","Huancavelica","Huánuco","Ica","Junín","La Libertad","Lambayeque","Lima","Loreto","Madre de Dios","Moquegua","Pasco","Piura",
@@ -71,37 +73,56 @@ public class MainActivity_2_Sites_NewSite extends AppCompatActivity {
             "Cusco","Huancavelica","Huánuco","Ica","Junín","La Libertad","Lambayeque","Lima","Loreto","Madre de Dios","Moquegua",
             "Pasco","Piura","Puno","San Martín","Tacna","Tumbes","Ucayali"};
     // Cambia a tus opciones reales
-    String[] provinceOptions = {"Province A", "Province B", "Province C"}; // Cambia a tus opciones reales
-    String[] districtOptions = {"District X", "District Y", "District Z"}; // Cambia a tus opciones reales
-    String[] zoneTypeOptions = {"Zone Type 1", "Zone Type 2", "Zone Type 3"}; // Cambia a tus opciones reales
-    String[] siteTypeOptions = {"Site Type 1", "Site Type 2", "Site Type 3"}; // Cambia a tus opciones reales
+    //String[] provinceOptions = {"Province A", "Province B", "Province C"}; // Cambia a tus opciones reales
+    //String[] districtOptions = {"District X", "District Y", "District Z"}; // Cambia a tus opciones reales
+    String[] zoneTypeOptions = {"Rural", "Urbano"}; // Cambia a tus opciones reales
+    String[] siteTypeOptions = {"Fijo", "Móvil"}; // Cambia a tus opciones reales
     FirebaseFirestore db;
     int resultSize;
     private ActivityResultLauncher<Intent> activityResultLauncher;
-
+    Map<String, String[]> provincesMap = new HashMap<>();
+    //Map<String, String[]> districtsMap = new HashMap<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_activity_main_new_site);
+        // Inicializar los Maps
+        initializeLocationData();
 
         // Encuentra las referencias a los campos de autocompletado
         selectDepartment = findViewById(R.id.selectDepartment);
         selectProvince = findViewById(R.id.selectProvince);
-        selectDistrict = findViewById(R.id.selectDistrict);
+
+        // Inicializa los adaptadores con las opciones
+        departmentAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, departmentOptions);
+        selectDepartment.setAdapter(departmentAdapter);
+
+        // Listener para el departamento
+        selectDepartment.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedDepartment = departmentOptions[position];
+            String[] provinces = provincesMap.get(selectedDepartment);
+            if (provinces != null) {
+                provinceAdapter = new ArrayAdapter<>(MainActivity_2_Sites_NewSite.this, android.R.layout.simple_dropdown_item_1line, provinces);
+                selectProvince.setAdapter(provinceAdapter);
+                selectProvince.setText("", false); // Clear previous selection
+            }
+        });
+
+        // Encuentra las referencias a los campos de autocompletado
+        selectDepartment = findViewById(R.id.selectDepartment);
+        selectProvince = findViewById(R.id.selectProvince);
         selectZoneType = findViewById(R.id.selectZoneType);
         selectSiteType = findViewById(R.id.selectSiteType);
 
         // Inicializa los adaptadores con las opciones
-        departmentAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, departmentOptions);
-        provinceAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, provinceOptions);
-        districtAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, districtOptions);
+        //departmentAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, departmentOptions);
+        //provinceAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, provinceOptions);
         zoneTypeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, zoneTypeOptions);
         siteTypeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, siteTypeOptions);
 
         // Asigna los adaptadores a los campos de autocompletado
         selectDepartment.setAdapter(departmentAdapter);
         selectProvince.setAdapter(provinceAdapter);
-        selectDistrict.setAdapter(districtAdapter);
         selectZoneType.setAdapter(zoneTypeAdapter);
         selectSiteType.setAdapter(siteTypeAdapter);
 
@@ -126,6 +147,7 @@ public class MainActivity_2_Sites_NewSite extends AppCompatActivity {
         imageView = findViewById(R.id.imageViewNewSite);
         imageView.setOnClickListener(v -> openFileChooser());
         editAddress = findViewById(R.id.editAddress);
+        editDistrict = findViewById(R.id.editDistrict);
         editUbigeo = findViewById(R.id.editUbigeo);
         editSiteCoordenadas = findViewById(R.id.editCoordenadas);
 
@@ -316,17 +338,40 @@ public class MainActivity_2_Sites_NewSite extends AppCompatActivity {
 
     private void saveSiteToFirestore(ListElementSite listElement, boolean isEditing) {
         Log.d("msg-test", "entro a saveSiteToFirestore");
+
+        if (listElement == null) {
+            Log.e("msg-test", "listElement es null");
+            return;
+        }
+
+        if (listElement.getName() == null) {
+            Log.e("msg-test", "listElement.getName() es null");
+            return;
+        }
+
+        Log.d("msg-test", "Guardando en Firestore: " + listElement.getName());
+
         db.collection("sitios")
                 .document(listElement.getName())
                 .set(listElement, SetOptions.merge())
                 .addOnSuccessListener(unused -> {
                     Log.d("msg-test", isEditing ? "Datos actualizados exitosamente" : "Data guardada exitosamente");
+                    // Mover la apertura de la nueva actividad aquí, dentro de onSuccessListener
+                    openSiteDetailsActivity(listElement);
                 })
-                .addOnFailureListener(e -> e.printStackTrace());
-        Intent intent3 = new Intent(MainActivity_2_Sites_NewSite.this, MainActivity_2_Sites_SiteDetails.class);
-        intent3.putExtra("ListElement", listElement);
-        startActivity(intent3);
+                .addOnFailureListener(e -> {
+                    Log.e("msg-test", "Error al guardar en Firestore", e);
+                    e.printStackTrace();
+                });
     }
+
+    private void openSiteDetailsActivity(ListElementSite listElement) {
+        Log.d("msg-test", "Abriendo MainActivity_2_Sites_SiteDetails con ListElementSite: " + listElement.getName());
+        Intent intent = new Intent(MainActivity_2_Sites_NewSite.this, MainActivity_2_Sites_SiteDetails.class);
+        intent.putExtra("ListElementSite", listElement);
+        startActivity(intent);
+    }
+
 
     private void createNewSite() {
         if (areFieldsEmpty() || !isImageAdded) {
@@ -334,7 +379,7 @@ public class MainActivity_2_Sites_NewSite extends AppCompatActivity {
         } else {
             String department = selectDepartment.getText().toString();
             String province = selectProvince.getText().toString();
-            String district = selectDistrict.getText().toString();
+            String district = editDistrict.getText().toString();
             String zonetype = selectZoneType.getText().toString();
             String sitetype = selectSiteType.getText().toString();
             String location = editSiteCoordenadas.getText().toString();
@@ -361,7 +406,7 @@ public class MainActivity_2_Sites_NewSite extends AppCompatActivity {
         } else {
             String department = selectDepartment.getText().toString();
             String province = selectProvince.getText().toString();
-            String district = selectDistrict.getText().toString();
+            String district = editDistrict.getText().toString();
             String zonetype = selectZoneType.getText().toString();
             String sitetype = selectSiteType.getText().toString();
             String location = editSiteCoordenadas.getText().toString();
@@ -385,7 +430,7 @@ public class MainActivity_2_Sites_NewSite extends AppCompatActivity {
     private boolean areFieldsEmpty() {
         return selectDepartment.getText().toString().isEmpty() ||
                 selectProvince.getText().toString().isEmpty() ||
-                selectDistrict.getText().toString().isEmpty() ||
+                editDistrict.getText().toString().isEmpty() ||
                 selectZoneType.getText().toString().isEmpty() ||
                 selectSiteType.getText().toString().isEmpty() ||
                 editAddress.getText().toString().isEmpty() ||
@@ -395,7 +440,7 @@ public class MainActivity_2_Sites_NewSite extends AppCompatActivity {
     private void fillFields(ListElementSite element) {
         selectDepartment.setText(element.getDepartment());
         selectProvince.setText(element.getProvince());
-        selectDistrict.setText(element.getDistrict());
+        editDistrict.setText(element.getDistrict());
         selectZoneType.setText(element.getZonetype());
         selectSiteType.setText(element.getSitetype());
         editSiteCoordenadas.setText(element.getCoordenadas());
@@ -458,6 +503,35 @@ public class MainActivity_2_Sites_NewSite extends AppCompatActivity {
         if (ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
             notificationManager.notify(1, notification);
         }
+    }
+
+    private void initializeLocationData() {
+        // Mapa de provincias por departamento
+        provincesMap.put("Amazonas", new String[]{"Chachapoyas", "Bagua", "Bongará", "Condorcanqui", "Luya", "Rodríguez de Mendoza", "Utcubamba"});
+        provincesMap.put("Áncash", new String[]{"Huaraz", "Aija", "Antonio Raymondi", "Asunción", "Bolognesi", "Carhuaz", "Carlos Fermín Fitzcarrald", "Casma", "Corongo", "Huari", "Huarmey", "Huaylas", "Mariscal Luzuriaga", "Ocros", "Pallasca", "Pomabamba", "Recuay", "Santa", "Sihuas", "Yungay"});
+        provincesMap.put("Apurímac", new String[]{"Abancay", "Andahuaylas", "Antabamba", "Aymaraes", "Cotabambas", "Chincheros", "Grau"});
+        provincesMap.put("Arequipa", new String[]{"Arequipa", "Camana", "Caraveli", "Castilla", "Caylloma", "Condesuyos", "Islay", "La Union"});
+        provincesMap.put("Ayacucho", new String[]{"Huamanga", "Cangallo", "Huanca Sancos", "Huanta", "La Mar", "Lucanas", "Parinacochas", "Páucar del Sara Sara", "Sucre", "Víctor Fajardo", "Vilcas Huamán"});
+        provincesMap.put("Cajamarca", new String[]{"Cajamarca", "Cajabamba", "Celendín", "Chota", "Contumazá", "Cutervo", "Hualgayoc", "Jaén", "San Ignacio", "San Marcos", "San Miguel", "San Pablo", "Santa Cruz"});
+        provincesMap.put("Callao", new String[]{"Callao"});
+        provincesMap.put("Cusco", new String[]{"Cusco", "Acomayo", "Anta", "Calca", "Canas", "Canchis", "Chumbivilcas", "Espinar", "La Convención", "Paruro", "Paucartambo", "Quispicanchi", "Urubamba"});
+        provincesMap.put("Huancavelica", new String[]{"Huancavelica", "Acobamba", "Angaraes", "Castrovirreyna", "Churcampa", "Huaytará", "Tayacaja"});
+        provincesMap.put("Huánuco", new String[]{"Huánuco", "Ambo", "Dos de Mayo", "Huacaybamba", "Huamalíes", "Leoncio Prado", "Marañón", "Pachitea", "Puerto Inca", "Lauricocha", "Yarowilca"});
+        provincesMap.put("Ica", new String[]{"Ica", "Chincha", "Nazca", "Palpa", "Pisco"});
+        provincesMap.put("Junín", new String[]{"Huancayo", "Concepción", "Chanchamayo", "Jauja", "Junín", "Satipo", "Tarma", "Yauli", "Chupaca"});
+        provincesMap.put("La Libertad", new String[]{"Trujillo", "Ascope", "Bolívar", "Chepén", "Julcán", "Otuzco", "Pacasmayo", "Pataz", "Sánchez Carrión", "Santiago de Chuco", "Gran Chimú", "Virú"});
+        provincesMap.put("Lambayeque", new String[]{"Chiclayo", "Ferreñafe", "Lambayeque"});
+        provincesMap.put("Lima", new String[]{"Lima", "Barranca", "Cajatambo", "Cañete", "Canta", "Huaral", "Huarochirí", "Huaura", "Oyón", "Yauyos"});
+        provincesMap.put("Loreto", new String[]{"Maynas", "Alto Amazonas", "Datem del Marañón", "Loreto", "Mariscal Ramón Castilla", "Putumayo", "Requena", "Ucayali"});
+        provincesMap.put("Madre de Dios", new String[]{"Tambopata", "Manu", "Tahuamanu"});
+        provincesMap.put("Moquegua", new String[]{"Mariscal Nieto", "General Sánchez Cerro", "Ilo"});
+        provincesMap.put("Pasco", new String[]{"Pasco", "Daniel Alcides Carrión", "Oxapampa"});
+        provincesMap.put("Piura", new String[]{"Piura", "Ayabaca", "Huancabamba", "Morropón", "Paita", "Sullana", "Talara", "Sechura"});
+        provincesMap.put("Puno", new String[]{"Puno", "Azángaro", "Carabaya", "Chucuito", "El Collao", "Huancané", "Lampa", "Melgar", "Moho", "San Antonio de Putina", "San Román", "Sandia", "Yunguyo"});
+        provincesMap.put("San Martín", new String[]{"Moyobamba", "Bellavista", "El Dorado", "Huallaga", "Lamas", "Mariscal Cáceres", "Picota", "Rioja", "San Martín", "Tocache"});
+        provincesMap.put("Tacna", new String[]{"Tacna", "Candarave", "Jorge Basadre", "Tarata"});
+        provincesMap.put("Tumbes", new String[]{"Tumbes", "Contralmirante Villar", "Zarumilla"});
+        provincesMap.put("Ucayali", new String[]{"Coronel Portillo", "Atalaya", "Padre Abad", "Purús"});
     }
 
 }
